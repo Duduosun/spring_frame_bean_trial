45bbe42ce316f4b84ffa3c4284d4063aad799c1a
Duduosun <tola.fasoyiro@hotmail.co.uk>
2015-08-20 23:38:03 +0100

    SecondFixForDependencyIssue_200815_2337

--- src/test/java/com/salmon/test/step_definitions/gui/smoke/SelfridgesCommonPageSteps.java
+++ src/test/java/com/salmon/test/step_definitions/gui/smoke/SelfridgesCommonPageSteps.java
@@ -1,27 +1,18 @@
 package com.salmon.test.step_definitions.gui.smoke;
 
 
-import com.salmon.test.page_objects.*;
+import com.salmon.test.page_objects.SelfridgesCommonPage;
 import cucumber.api.java.en.When;
 
 /**
  * Created by Tola on 17/08/2015
  */
 public class SelfridgesCommonPageSteps {
-    private GridViewPage gridViewPage;
-    private MyDetailsPage myDetailsPage;
-    private SelfridgesCommonPage selfridgesCommonPage;
-    private SelfridgesHomePage selfridgesHomePage;
-    private SignInPage signInPage;
 
-    public SelfridgesCommonPageSteps(GridViewPage gridViewPage, MyDetailsPage myDetailsPage,
-                                 SelfridgesCommonPageSteps selfridgesCommonPageSteps,SelfridgesHomePage selfridgesHomePage,
-                                 SignInPage signInPage){
-        this.gridViewPage = gridViewPage;
-        this.myDetailsPage = myDetailsPage;
+    private SelfridgesCommonPage selfridgesCommonPage;
+
+    public SelfridgesCommonPageSteps(SelfridgesCommonPage selfridgesCommonPage){
         this.selfridgesCommonPage = selfridgesCommonPage;
-        this.selfridgesHomePage = selfridgesHomePage;
-        this.signInPage = signInPage;
     }
 
 
--- src/test/resources/features/web/Desktop/RT004_Bulk_PDP.feature
+++ src/test/resources/features/web/Desktop/RT004_Bulk_PDP.feature
@@ -16,5 +16,5 @@
         | MegaMenuSix |  |  |
         | MegaMenuSeven |  |  |
         | MegaMenuEight |  |  |
-        | MegaMenuNine |  |  |
+       # | MegaMenuNine |  |  |
 
